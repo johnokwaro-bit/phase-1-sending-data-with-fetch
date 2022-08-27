3b0462c48447658933e384b335791794b157ed30
John Okwaro <john.okwaro@moringaschool.com>
2022-08-27 14:02:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(userName,userEmail){
+    const data = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configurationObject ={
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body:JSON.stringify(data)
+        }
+    let myFetch = fetch("http://localhost:3000/users", configurationObject)        
+        .then(myFetch => myFetch.json())         
+        .then(object => {const id = object["id"];
+        const body = document.querySelector("body");
+        body.innerHTML = id;
+        console.log(body)
+        
+    })
+
+
+    .catch(function(error){
+        let message = "Unauthorized Access"
+        alert(message);
+        document.body.innerHTML= message
+    })
+    return myFetch;
+}
+submitData("Steve", "steve@steve.com")
+
+
